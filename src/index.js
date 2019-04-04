54a85ceca319e5b542d43f1778dc83603c877f0d
mrc <marcoambrosini@pm.me>
2019-04-04 12:26:24 +0200

    fetch implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./index.css"
 import * as d3 from "d3"
 
-const square = d3.selectAll("rect")
-square.style("fill", "blue")
+fetch(
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+)
+  .then(response => response.json())
+  .then(data => {
+    let DATA = data
+    d3DisplayData(DATA)
+  })
+
+let d3DisplayData = DATA => {
+  console.log("this is the data", DATA)
+}
